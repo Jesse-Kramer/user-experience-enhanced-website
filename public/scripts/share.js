2dedf5e2523c81b2e5d6cb281873c7cd69f12bac
Jesse Kramer <jessekramer15@gmail.com>
2024-04-26 10:46:25 +0200

    Fix

--- public/scripts/share.js
+++ public/scripts/share.js
@@ -1,24 +1,24 @@
-/* Client sided share function */
+// Get the post and category slug from the hidden input field
+const postSlugInput = document.querySelector('input[name="slug"]');
+const categorySlugInput = document.querySelector('input[name="category"]');
+const shareBtn = document.getElementById("share-button");
+ 
  
 //Add an event listener to the share button
-document.getElementById("share-button").addEventListener("click", async(event) => {
+shareBtn.addEventListener("click", async(event) => {
  
-    // Get the post and category slug from the hidden input field
-    const postSlugInput = document.querySelector('input[name="slug"]');
-    const postSlug = postSlugInput.value;
-    const categorySlugInput = document.querySelector('input[name="category"]');
-    const categorySlug = categorySlugInput.value;
- 
+    event.preventDefault(); 
+
     try {
         // Send a POST request to the server to share the post
-        const response = await fetch(`/${categorySlug}/${postSlug}`, {
+        const response = await fetch(`/${categorySlugInput.value}/${postSlugInput.value}`, {
             method: 'POST',
             headers: {
                 // Setting the request header to specify JSON content
                 'Content-Type': 'application/json'
             },
             // Sending the post slug as JSON in the request body
-            body: JSON.stringify({ slug: postSlug })
+            body: JSON.stringify({ slug: postSlugInput.value })
         });
  
         // If the response is successful
@@ -35,6 +35,4 @@ document.getElementById("share-button").addEventListener("click", async(event) =
     } catch (error) {
         console.error('Error:', error);
     }
-
-        event.preventDefault();
 });
