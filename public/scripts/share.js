dfe49cb5ad2612b58e04dd65ad0376a249e0128c
OvenMagnetron <jessekramer15@gmail.com>
2024-05-07 17:29:16 +0200

    Added lazy loading and fixed share function

--- public/scripts/share.js
+++ public/scripts/share.js
@@ -5,34 +5,26 @@ const shareBtn = document.getElementById("share-button");
  
  
 //Add an event listener to the share button
-shareBtn.addEventListener("click", async(event) => {
- 
-    event.preventDefault(); 
+shareBtn.addEventListener("click", (event) => {
 
-    try {
-        // Send a POST request to the server to share the post
-        const response = await fetch(`/${categorySlugInput.value}/${postSlugInput.value}`, {
-            method: 'POST',
-            headers: {
-                // Setting the request header to specify JSON content
-                'Content-Type': 'application/json'
-            },
-            // Sending the post slug as JSON in the request body
-            body: JSON.stringify({ slug: postSlugInput.value })
-        });
- 
-        // If the response is successful
-        if (response.ok) {
-            // Update the share count displayed on the client side
-            const shareCountElement = document.getElementById("shareCount");
-            // Incrementing the share count displayed on the client side
-            shareCountElement.innerText = parseInt(shareCountElement.innerText) + 1;
-            
- 
-        } else {
-            console.error('Error:', response.statusText);
-        }
-    } catch (error) {
+    // Send a POST request to the server to share the post
+    fetch(`/${categorySlugInput.value}/${postSlugInput.value}`, {
+        method: 'POST',
+        headers: {
+            // Setting the request header to specify JSON content
+            'Content-Type': 'application/json'
+        },
+        // Sending the post slug as JSON in the request body
+        body: JSON.stringify({ slug: postSlugInput.value })
+    })
+    .then(response => {
+        // Update the share count displayed on the client side
+        const shareCountElement = document.getElementById("shareCount");
+        // Incrementing the share count displayed on the client side
+        shareCountElement.innerText = parseInt(shareCountElement.innerText) + 1;
+    })
+    .catch(error => {
         console.error('Error:', error);
-    }
+    });
+    event.preventDefault();
 });
